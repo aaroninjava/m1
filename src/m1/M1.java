44f93ad3895015d9edc18957cfb258d45d27c250
A <A@PC106>
2018-01-28 20:56:58 +0800

    Log

---
+++ src/m1/M1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m1;
+
+/**
+ *
+ * @author A
+ */
+public class M1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here        
+        double x,y;
+        x=10;
+        //y=Math.sqrt(x);
+        //System.out.println("sqr("+x+")="+y);
+        
+        //y=Math.abs(x);        
+        //System.out.println("abs("+x+")="+y);
+        
+        
+        //y=Math.exp(x);        
+        //System.out.println("exp("+x+")="+y);
+        
+        //y=Math.pow(10,x);        
+        //System.out.println("pow(10,x) ="+y);
+    
+        y=Math.log(x);        
+        System.out.println("ln(x) ="+y);
+        
+        
+    }
+    
+    
+}
