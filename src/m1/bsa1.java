6b8f59a76a192a5ef300cad3ee5f5c55bb2d9982
A <A@PC106>
2018-01-31 21:24:23 +0800

    class_greeks_gamma

--- src/m1/bsa1.java
+++ src/m1/bsa1.java
@@ -2,8 +2,9 @@ package m1;
 import org.apache.commons.math3.distribution.NormalDistribution;
 public class bsa1 {
     final static double api=Math.PI;  
-    double x;
+    double x,d1,d2,pdfa,cdfa;    
     double S,K,T,sigma,r,y;  
+    NormalDistribution n1 = new NormalDistribution();    
     
     public bsa1(){}
     public bsa1(double x){}
@@ -13,8 +14,88 @@ public class bsa1 {
         this.T=T;
         this.sigma=sigma;
         this.r=r;
-        this.y=y;        
+        this.y=y;
+        d1 =( Math.log(S/K) + 
+                (r-y + sigma * sigma / 2)* T ) / (sigma * Math.sqrt(T));
+        d2 = d1- sigma * Math.sqrt(T) ;    
+        
     }
+          
+    public double cdf(double x)
+    {                     
+        double cdf1;                
+        cdf1=n1.cumulativeProbability(x);
+        return cdf1;        
+    }
+    
+    public double pdf(double x)
+    {                     
+        double pdf1;                
+        pdf1=n1.density(x);
+        return pdf1;        
+    }
+    
+    //Op_Cvalue = S * NorCdf(d1) - K * Exp(-r * T) * NorCdf(d2)
+    public double call()
+    {                     
+        double calla;                
+        calla=S*cdf(d1)- K*Math.exp(-r*T)* cdf(d2);        
+        return calla;        
+    }
+    
+    //Op_Pvalue = K * Exp(-r * T) * NorCdf(-d2) - S * NorCdf(-d1)
+    public double put()
+    {                     
+        double puta;                
+        puta=K*Math.exp(-r*T)* cdf(-d2) - S*cdf(-d1);         
+        return puta;        
+    }
+    
+    //cdelta = Math.exp(-y * T) * b1.cdfapa(d1);
+    //public double deltaC() 
+    public double delta(String CP) 
+    {
+        double delta1;
+        if (CP=="C")
+            {delta1 = Math.exp(-y * T) * cdf(d1);}
+        else
+            {delta1 = Math.exp(-y * T) * ( cdf(d1)-1 );}
+        System.out.println("delta="+delta1); 
+        return delta1;
+    }
+        
+    public double gamma() //call.put相同
+    {
+        double gamma1;
+        gamma1 = Math.exp(-y * T) * pdf(d1)/(S * sigma * Math.sqrt(T));
+        System.out.println("gamma="+gamma1); 
+        return gamma1;
+    }
+    
+    public double vega() //call.put相同
+    {
+        double vega1=0;
+        //gamma1 = Math.exp(-y * T) * pdf(d1)/(S * sigma * Math.sqrt(T));
+        System.out.println("gamma="+vega1); 
+        return vega1;
+    }
+    
+    public double theta() //call.put相同
+    {
+        double gamma1;
+        gamma1 = Math.exp(-y * T) * pdf(d1)/(S * sigma * Math.sqrt(T));
+        System.out.println("theta="+gamma1); 
+        return gamma1;
+    }
+    
+    public double rho() 
+    {
+        double gamma1;
+        gamma1 = Math.exp(-y * T) * pdf(d1)/(S * sigma * Math.sqrt(T));
+        System.out.println("rho="+gamma1); 
+        return gamma1;
+    }
+    
     
 //public void double xx(double x) 
     public double xx() 
@@ -29,14 +110,13 @@ public class bsa1 {
     } 
     
     //public static void dd1(double S,double K,double T,double sigma,double r,double y) 
-    public double dd1() 
+    /*public double dd1() 
     {
-        double d1,d2;
-        d1 =( Math.log(S/K) + (r-y + sigma * sigma / 2)* T ) / (sigma * Math.sqrt(T));          
-        System.out.println("dd1="+d1); 
-        return d1;
-    }   
+        double d1a;
+        d1a =( Math.log(S/K) + (r-y + sigma * sigma / 2)* T ) / (sigma * Math.sqrt(T));          
+        System.out.println("dd1="+d1a); 
+        return d1a;
+    } */  
     
-
     
 }
